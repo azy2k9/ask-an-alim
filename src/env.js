65ff2dac4836e77977eaee4fd61bdae34408d851
azy2k9 <azy2k9@gmail.com>
2024-07-11 12:57:51 +0100

    Fix environment variables

--- src/env.js
+++ src/env.js
@@ -42,6 +42,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
