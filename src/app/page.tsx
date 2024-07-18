ff0c2e435364531883e604f56be174e9d8012e61
azy2k9 <azy2k9@gmail.com>
2024-07-18 19:29:15 +0100

    Cleanup and start the credentials auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,22 +17,29 @@ export default async function Home() {
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Ask <span className="text-[hsl(280,100%,70%)]">An</span> Alim
           </h1>
-          <AskQuestion />
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            {questions.length ? (
-              questions.map((q) => (
-                <QAndA key={q.id} question={q.question} answer={q.answer} />
-              ))
-            ) : (
-              <div>There are no questions here.</div>
-            )}
+          <div className="flex">
+            <AskQuestion />
+            <Link
+              href={session ? "/api/auth/signout" : "/sign-in"}
+              className="mx-2 rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+            >
+              {session ? "Sign out" : "Sign in"}
+            </Link>
           </div>
-          <Link
-            href={session ? "/api/auth/signout" : "/sign-in"}
-            className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-          >
-            {session ? "Sign out" : "Sign in"}
-          </Link>
+          {questions.length ? (
+            questions.map((q) => (
+              <div
+                key={q.id}
+                className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8"
+              >
+                <QAndA question={q.question} answer={q.answer} />
+              </div>
+            ))
+          ) : (
+            <div className="flex justify-center">
+              There hasnt been any questions yet!
+            </div>
+          )}
           {/* <div className="flex flex-col items-center gap-2">
             <div className="flex flex-col items-center justify-center gap-4">
               <p className="text-center text-2xl text-white">
@@ -46,7 +53,6 @@ export default async function Home() {
               </Link>
             </div>
           </div> */}
-          {/* {session?.user && <LatestQuestions />} */}
         </div>
       </main>
     </HydrateClient>
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -12,6 +12,7 @@ import { useRouter } from "next/navigation";
 
 const Signin = () => {
   const router = useRouter();
+
   const { handleSubmit, control } = useForm<SigninForm>({
     resolver: zodResolver(SigninSchema),
     mode: "onBlur",
@@ -20,8 +21,17 @@ const Signin = () => {
   return (
     <form
       className="flex flex-col w-full"
-      onSubmit={handleSubmit((d) => {
-        console.log({ email: d.email, password: d.password });
+      onSubmit={handleSubmit(async ({ email, password }) => {
+        const response = await signIn("credentials", {
+          email,
+          password,
+          redirect: false,
+        });
+
+        if (!response?.error && response?.ok) {
+          router.push("/");
+          router.refresh();
+        }
       })}
     >
       <FormField name="email" placeholder="Email..." control={control} />
@@ -31,15 +41,15 @@ const Signin = () => {
         type="password"
         control={control}
       />
-      <div className="md:px-16 align-middle">
+      <div className="md:px-60 align-middle">
         <button
           type="submit"
-          className="btn bg-purple-300 text-white rounded-lg justify-right w-full mt-2 p-2"
+          className="btn bg-purple-400 hover:bg-purple-500 text-white rounded-lg justify-right w-full mt-2 p-2"
         >
           Sign in
         </button>
         <button
-          className="btn bg-purple-300 text-white flex rounded-lg justify-center w-full my-2 p-2"
+          className="btn bg-purple-400 hover:bg-purple-500 text-white flex rounded-lg justify-center w-full my-2 p-2"
           onClick={async (e) => {
             e.preventDefault();
             await signIn("google", {
@@ -51,7 +61,7 @@ const Signin = () => {
           <span>Sign in with Google</span>
         </button>
         <button
-          className="flex text-white bg-purple-400 hover:bg-purple-500 rounded-lg justify-center w-full my-2 p-2"
+          className="flex text-white bg-purple-500 hover:bg-purple-600 rounded-lg justify-center w-full my-2 p-2"
           onClick={async (e) => {
             e.preventDefault();
             router.push("/sign-up");
